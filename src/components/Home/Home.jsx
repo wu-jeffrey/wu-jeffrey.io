1d09d03b9af88b41aba174552eceaee21f7c3cb6
wu-jeffrey <jeff.jiang.wu@gmail.com>
2021-01-07 15:41:30 -0500

    geometric art done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Layout, Button, Space } from 'antd';
-import {
-  GithubOutlined,
-  LinkedinOutlined
-} from '@ant-design/icons';
+import { Button, Space } from 'antd';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import { motion, AnimatePresence } from 'framer-motion'
 
 import './Home.css'
+import { GeometricArt } from '../ui/GeometricArt'
 
 export function Home() {
   return (
@@ -13,26 +12,31 @@ export function Home() {
       <div className='header bold'>
         JW.
       </div>
-      <div className='hero'>
-        <div className="left">
-          <h1 className='bold'>Jeffrey Wu</h1>
-          <h4>Fullstack Developer</h4>
-          <h4>Engineering Student</h4>
-          <h4>DIY Addict</h4>
-          <Space>
-            <Button
-              href="https://www.linkedin.com/in/jeff--wu/"
-              type="primary"
-              shape="circle"
-              icon={<LinkedinOutlined />} />
-            <Button
-              href="https://github.com/wu-jeffrey"
-              shape="circle"
-              icon={<GithubOutlined />} />
-          </Space>
-        </div>
-        <div className="right">
 
+      <div className='hero'>
+        <AnimatePresence>
+          <motion.div className="left"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}>
+            <h1 className='bold'>Jeffrey Wu</h1>
+            <h4>Fullstack Developer</h4>
+            <h4>Engineering Student</h4>
+            <h4>DIY Addict</h4>
+            <Space>
+              <Button
+                href="https://www.linkedin.com/in/jeff--wu/"
+                type="primary"
+                shape="circle"
+                icon={<LinkedinOutlined />} />
+              <Button
+                href="https://github.com/wu-jeffrey"
+                shape="circle"
+                icon={<GithubOutlined />} />
+            </Space>
+          </motion.div>
+        </AnimatePresence>
+        <div className="right">
+          <GeometricArt />
         </div>
       </div>
     </div>
---
+++ src/components/ui/GeometricArt.js
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion'
+
+export function GeometricArt() {
+  const [height, setHeight] = useState(400)
+  const [width, setWidth] = useState(400)
+
+
+  const generateDonuts =
+    ['#3700B3', '#018786', '#FFDE03', 'rgb(24, 144,255)', '#FF0266'].map(
+      (color) => {
+        const randomX = Math.floor(Math.floor(Math.random() * (width - 100)) + 50)
+        const randomY = Math.floor(Math.floor(Math.random() * (height - 100)) + 50)
+
+        const t0 = Math.random() * 0.33;
+        const t1 = t0 + Math.random() * 0.33;
+
+        return (
+          <motion.circle cx={randomX} cy={randomY} r="10" width="100%" height="100%" fill="none" stroke={color} key={color}
+            animate={{
+              strokeWidth: [0, 10, 0],
+              opacity: [0, 1, 0]
+            }}
+            transition={{
+              delay: Math.floor(Math.random() * 5),
+              duration: 2,
+              ease: "easeOut",
+              times: [t0, t1, 1],
+              loop: Infinity,
+              repeatDelay: Math.floor(Math.random() * 5),
+            }} />
+        )
+      }
+    )
+
+  const generateShootingStars =
+    ['#3700B3', '#018786', '#FFDE03', 'rgb(24, 144,255)', '#FF0266'].map(
+      (color, index) => {
+
+        const slope = 1;
+        // Length needs to be dynamic if we're doing resizing of the svg
+        const length = 396;
+
+        // generate random at the bottom edges, i.e. bigX bigY, midX bigY, bigX, midY
+        const p0 = {
+          x: Math.floor(Math.random() * (width * 0.15)) + width * 0.8,
+          y: Math.floor(Math.random() * (height * 0.15)) + height * 0.8
+        }
+
+        const dx =
+          Math.sqrt(
+            Math.pow(length, 2) / (1 + Math.pow(slope, 2))
+          )
+
+        const dy =
+          Math.sqrt(
+            Math.pow(length, 2) - Math.pow(dx, 2)
+          )
+
+        const p1 = {
+          x: Math.floor(p0.x - dx),
+          y: Math.floor(p0.y - dy)
+        }
+
+        return (
+          <motion.path d={`M${p0.x} ${p0.y}, L${p1.x} ${p1.y}`} stroke={color} fill="none" strokeWidth="10" strokeLinecap="round" key={index}
+            animate={{
+              opacity: [0, 0.5, 1],
+              // Dasharray specifies the [dashLength gapLength], we want the gapL to be huge cuz we want to see only one dash
+              strokeDasharray: [`${length} 1000`, "30 1000"],
+              // Dashoffset specifies the offset of the dash from the beginning of the path
+              // we want start at -200 at the start or else it looks like the line is shrinking into a single point
+              // we want end at length so that the whole dash gets pushed off the screen
+              strokeDashoffset: [-200, length],
+            }}
+            transition={{
+              duration: 1,
+              ease: 'easeIn',
+              loop: Infinity,
+              delay: index + Math.random(),
+              repeatDelay: index + Math.random(),
+            }} />
+        )
+      }
+    )
+
+  return (
+    <motion.div style={{ zIndex: -10, marginLeft: 32 }} initial={{ opacity: 0, rotate: 50 }} animate={{ opacity: 1, rotate: 0 }}>
+      <motion.svg height={height} width={width}>
+        <defs>
+          <pattern id="diagonalHatch" width="10" height="10" patternTransform="rotate(20 0 0)" patternUnits="userSpaceOnUse">
+            <line x1="0" y1="0" x2="0" y2="10" style={{ stroke: 'rgb(24, 144,255)', strokeWidth: 10 }} />
+          </pattern>
+          <filter id="glow" x="-30%" y="-30%" width="160%" height="160%">
+            <feGaussianBlur stdDeviation="10 10" result="glow" />
+            <feMerge>
+              <feMergeNode in="glow" />
+            </feMerge>
+          </filter>
+        </defs>
+        <motion.g
+          animate={{ translateY: [0, -20, 0], rotate: [3, -2, 3] }}
+          transition={{
+            duration: 5,
+            ease: 'easeInOut',
+            times: [0, 0.6, 1],
+            loop: Infinity,
+          }}>
+          <motion.polygon points="60 50, 300 50, 180 300" fill="rgb(24, 144,255)" style={{ filter: 'url(#glow)' }} />
+          <motion.polygon points="60 50, 300 50, 180 300" fill="rgb(24, 144,255)" />
+        </motion.g>
+        <motion.g
+          animate={{ translateY: [0, -50, 0], rotate: [20, -20, 20] }}
+          transition={{
+            duration: 3,
+            ease: 'easeInOut',
+            loop: Infinity,
+          }}>
+          <motion.rect x="300" y="300" rx="10" width="60" height="60" fill="#00ffbf" style={{ filter: 'url(#glow)' }} />
+          <motion.rect x="300" y="300" rx="10" width="60" height="60" fill="#00ffbf" />
+        </motion.g>
+        <motion.g
+          animate={{
+            translateY: [0, 40, 0],
+            rotate: [-3, 2, -3],
+          }}
+          transition={{
+            duration: 6,
+            ease: 'easeInOut',
+            times: [0, 0.6, 1],
+            loop: Infinity,
+            repeatDelay: 0.25,
+          }} >
+          <motion.text x={"50"} y={"250"} rotate={-10} fill="grey" style={{ filter: 'url(#glow)', fontSize: 200, fontWeight: 'bold' }}>J</motion.text>
+          <motion.text x={"50"} y={"250"} rotate={-10} fill="#EB4B4B" style={{ fontSize: 200, fontWeight: 'bold' }}>J</motion.text>
+        </motion.g>
+        <motion.g
+          animate={{
+            translateY: [0, 50, 0],
+            rotate: [4, -8, 4],
+          }}
+          transition={{
+            duration: 7,
+            ease: 'easeInOut',
+            times: [0, 0.4, 1],
+            loop: Infinity,
+            repeatDelay: 0.3,
+          }} >
+          <motion.text x={"220"} y={"250"} rotate={-10} fill="grey" style={{ filter: 'url(#glow)', fontSize: 200, fontWeight: 'bold' }}>W</motion.text>
+          <motion.text x={"220"} y={"250"} rotate={-10} fill="#EB4B4B" style={{ fontSize: 200, fontWeight: 'bold' }}>W</motion.text>
+        </motion.g>
+        {generateDonuts}
+        {generateShootingStars}
+      </motion.svg>
+    </motion.div >
+  )
+}
